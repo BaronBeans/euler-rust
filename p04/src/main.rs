d607319fcea8f20502b9bbcb3ba44a72358c6643
Sean Barnard <sean.barnard.94@hotmail.com>
2022-07-10 21:51:56 +0100

    add 4, 5 and 6, done 4 and 6 was easy.

--- p04/src/main.rs
+++ p04/src/main.rs
@@ -4,5 +4,35 @@ A palindromic number reads the same both ways. The largest palindrome made from 
 Find the largest palindrome made from the product of two 3-digit numbers.
 */
 fn main() {
-    println!("Hello, world!");
+    let mut max: u64 = 0;
+    for x in 100..1000 {
+        for y in 100..1000 {
+            let prod = x * y;
+            let pal = is_palindrome(&prod.to_string());
+            if pal {
+                if prod > max {
+                    max = prod;
+                };
+            };
+        };
+    };
+    println!("{}", max);
+}
+
+fn is_palindrome(input: &str) -> bool {
+    let forwards: Vec<char> = input.chars().collect();
+    let backwards: Vec<char> = input.chars().rev().collect();
+    backwards == forwards
+}
+
+#[test]
+fn test_is_palindrome() {
+    assert_eq!(is_palindrome("101"), true);
+    assert_eq!(is_palindrome("1001"), true);
+    assert_eq!(is_palindrome("10101"), true);
+    assert_eq!(is_palindrome("99999"), true);
+
+    assert_eq!(is_palindrome("99998"), false);
+    assert_eq!(is_palindrome("1234"), false);
+    assert_eq!(is_palindrome("1234"), false);
 }
---
+++ p06/src/main.rs
@@ -0,0 +1,49 @@
+/*
+The sum of the squares of the first ten natural numbers is,
+1^2 + 2^2 +...+ 10^2 = 385
+
+The square of the sum of the first ten natural numbers is,
+(1 + 2 +...+ 10)^2 = 55^2 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is
+3025 - 385 = 2640
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+*/
+
+fn main() {
+    println!("{}", find_answer(100));
+}
+
+fn find_answer(l: u64) -> u64 {
+    let sum_of_squares = sum_of_square(l);
+    let square_of_sums = square_of_sum(l);
+
+    square_of_sums - sum_of_squares
+}
+
+fn sum_of_square(l: u64) -> u64 {
+    let mut count: u64 = 0;
+    for i in 1..=l {
+        count += i*i;
+    };
+    count
+}
+
+fn square_of_sum(l: u64) -> u64 {
+    let mut sum: u64 = 0;
+    for i in 1..=l {
+        sum += i;
+    };
+    sum * sum
+}
+
+#[test]
+fn test_sum_of_square() {
+    assert_eq!(sum_of_square(10), 385);
+}
+
+#[test]
+fn test_square_of_sum() {
+    assert_eq!(square_of_sum(10), 3025);
+}
