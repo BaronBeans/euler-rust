4de56448f503fdac4d526f6e1a51e99de0fc250e
Sean Barnard <sean.barnard.94@hotmail.com>
2022-07-10 21:56:36 +0100

    started p07

---
+++ p07/src/main.rs
@@ -0,0 +1,18 @@
+/*
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+*/
+
+fn main() {
+    println!("Hello, world!");
+}
+
+fn get_prime_at(i: u64) -> u64 {
+    13
+}
+
+#[test]
+fn test_case() {
+    assert_eq!(get_prime_at(6), 13);
+}
