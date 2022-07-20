7f5e798f0ba172ecfa4f3723f03c14a97672969e
Sean Barnard <sean.barnard.94@hotmail.com>
2022-07-20 22:15:30 +0100

    completed p05

--- p05/src/main.rs
+++ p05/src/main.rs
@@ -4,10 +4,57 @@
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 */
 fn main() {
-    println!("Hello, world!");
+    find_number_10();
+    println!("{}", find_number_20());
+}
+
+fn find_number_10() -> u64 {
+    let mut i: u64 = 1;
+    while i % 10 != 0
+        || i % 9 != 0
+        || i % 8 != 0
+        || i % 7 != 0
+        || i % 6 != 0
+        || i % 5 != 0
+        || i % 4 != 0
+        || i % 3 != 0
+        || i % 2 != 0
+        || i % 1 != 0
+    {
+        i += 1;
+    }
+    i
+}
+
+fn find_number_20() -> u64 {
+    let mut i: u64 = 1;
+    while i % 20 != 0
+        || i % 19 != 0
+        || i % 18 != 0
+        || i % 17 != 0
+        || i % 16 != 0
+        || i % 15 != 0
+        || i % 14 != 0
+        || i % 13 != 0
+        || i % 12 != 0
+        || i % 11 != 0
+        || i % 10 != 0
+        || i % 9 != 0
+        || i % 8 != 0
+        || i % 7 != 0
+        || i % 6 != 0
+        || i % 5 != 0
+        || i % 4 != 0
+        || i % 3 != 0
+        || i % 2 != 0
+        || i % 1 != 0
+    {
+        i += 1;
+    }
+    i
 }
 
 #[test]
 fn test_case() {
-    assert_eq!(true, true);
+    assert_eq!(find_number_10(), 2520);
 }
